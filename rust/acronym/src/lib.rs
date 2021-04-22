7cdc16267eaf7c7155321e527a08871159b8d629
Phillip Cloud <cloud@standard.ai>
2021-04-22 07:55:55 -0400

    Add acronym and solution

---
+++ rust/acronym/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn abbreviate(phrase: &str) -> String {
+    phrase
+        .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
+        .flat_map(|word| {
+            word.chars()
+                .take(1)
+                .chain(
+                    word.chars()
+                        .skip_while(|c| c.is_uppercase())
+                        .filter(|c| c.is_uppercase()),
+                )
+                .flat_map(|c| c.to_uppercase())
+        })
+        .collect()
+}
