0beec82e71f0f5650b8d041fe80e1d9a3fa519d3
Phillip Cloud <cloud@standard.ai>
2021-04-24 06:04:51 -0400

    More plf

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,6 +1,7 @@
 mod stdlib_impl {
     use std::{collections::HashMap, sync::mpsc};
 
+    #[allow(dead_code)]
     pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         let (producer, consumer) = mpsc::channel::<HashMap<_, usize>>();
 
@@ -48,12 +49,12 @@ mod stdlib_impl {
                         .send(chunk.into_iter().fold(
                             Default::default(),
                             move |mut counts, string| {
-                                for c in string
-                                    .chars()
-                                    .filter(|c| c.is_alphabetic())
-                                    .flat_map(|c| c.to_lowercase())
-                                {
-                                    *counts.entry(c).or_default() += 1;
+                                for c in string.chars() {
+                                    if c.is_alphabetic() {
+                                        for lc in c.to_lowercase() {
+                                            *counts.entry(lc).or_default() += 1;
+                                        }
+                                    }
                                 }
                                 counts
                             },
@@ -84,58 +85,56 @@ mod stdlib_impl {
 }
 
 mod crossbeam_impl {
-    use crossbeam::thread;
     use std::{collections::HashMap, sync::mpsc};
 
     // This implementation is the same as the `raw` implementation, except that it
     // uses scoped threads to avoid copying the input.
+    #[allow(dead_code)]
     pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         let (producer, consumer) = mpsc::channel::<HashMap<_, usize>>();
         let (result_tx, result_rx) = mpsc::sync_channel(1);
+        let nchunks = {
+            let nchunks = input.len() / worker_count;
+            if nchunks == 0 {
+                input.len().max(1)
+            } else {
+                nchunks
+            }
+        };
 
-        thread::scope(move |scope| {
+        crossbeam::thread::scope(move |scope| {
             let consumer_thread = scope.spawn(move |_| {
                 let mut freq = HashMap::new();
                 while let Ok(map) = consumer.recv() {
-                    for (c, count) in map.into_iter() {
-                        *freq.entry(c).or_default() += count;
+                    for (ch, count) in map.into_iter() {
+                        *freq.entry(ch).or_default() += count;
                     }
                 }
 
                 result_tx.send(freq).unwrap();
             });
 
-            let nchunks = input.len() / worker_count;
             input
-                .chunks(if nchunks == 0 {
-                    input.len().max(1)
-                } else {
-                    nchunks
-                })
+                .chunks(nchunks)
                 .map(|chunk| {
                     let producer_clone = producer.clone();
 
                     scope.spawn(move |_| {
-                        producer_clone
-                            .send(chunk.iter().fold(
-                                Default::default(),
-                                move |mut counts, string| {
-                                    for c in string
-                                        .chars()
-                                        .filter(|c| c.is_alphabetic())
-                                        .flat_map(|c| c.to_lowercase())
-                                    {
-                                        *counts.entry(c).or_default() += 1;
-                                    }
-                                    counts
-                                },
-                            ))
-                            .unwrap();
+                        let mut counts = HashMap::new();
+                        for c in chunk.iter().flat_map(|&string| string.chars()) {
+                            if c.is_alphabetic() {
+                                for lc in c.to_lowercase() {
+                                    *counts.entry(lc).or_default() += 1;
+                                }
+                            }
+                        }
+
+                        producer_clone.send(counts).unwrap();
                     })
                 })
                 .collect::<Vec<_>>()
                 .into_iter()
-                .for_each(|thread| thread.join().unwrap());
+                .for_each(|producer_thread| producer_thread.join().unwrap());
 
             drop(producer);
 
@@ -150,6 +149,7 @@ mod rayon_impl {
     use rayon::iter::{IntoParallelIterator, ParallelIterator};
     use std::collections::HashMap;
 
+    #[allow(dead_code)]
     pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         rayon::ThreadPoolBuilder::new()
             .num_threads(worker_count)
@@ -158,13 +158,13 @@ mod rayon_impl {
             .scope(|_scope| {
                 input
                     .into_par_iter()
-                    .fold(HashMap::new, |mut counts, &c| {
-                        for lower_char in c
-                            .chars()
-                            .filter(|c| c.is_alphabetic())
-                            .flat_map(|c| c.to_lowercase())
-                        {
-                            *counts.entry(lower_char).or_default() += 1;
+                    .fold(HashMap::new, |mut counts, &string| {
+                        for ch in string.chars() {
+                            if ch.is_alphabetic() {
+                                for lc in ch.to_lowercase() {
+                                    *counts.entry(lc).or_default() += 1;
+                                }
+                            }
                         }
                         counts
                     })
