735624c191ec2d3dca17023a25c3617cb6f70353
Phillip Cloud <cpcloud@gmail.com>
2021-05-05 13:32:51 -0400

    Implement say

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -1,4 +1,5 @@
-use std::{convert::TryFrom, str::FromStr};
+use itertools::{EitherOrBoth, Itertools};
+use std::str::FromStr;
 
 fn ones(n: u64) -> Option<&'static str> {
     Some(match n {
@@ -17,7 +18,6 @@ fn ones(n: u64) -> Option<&'static str> {
 
 fn teens(n: u64) -> Option<&'static str> {
     Some(match n {
-        10 => "ten",
         11 => "eleven",
         12 => "twelve",
         13 => "thirteen",
@@ -31,16 +31,9 @@ fn teens(n: u64) -> Option<&'static str> {
     })
 }
 
-fn low_numbers(n: u64) -> Option<String> {
-    match n {
-        1..=9 => return ones(n).map(ToOwned::to_owned),
-        10..=19 => return teens(n).map(ToOwned::to_owned),
-        _ => None,
-    }
-}
-
 fn tens(n: u64) -> Option<&'static str> {
     Some(match n {
+        10 => "ten",
         20 => "twenty",
         30 => "thirty",
         40 => "forty",
@@ -53,8 +46,64 @@ fn tens(n: u64) -> Option<&'static str> {
     })
 }
 
-const GROUPS: [&str; 7] = [
-    "",
+fn simple(n: u64) -> String {
+    let hundreds_digit = n / 100;
+    let tens_digit = (n - 100 * hundreds_digit) / 10;
+    let ones_digit = n % 10;
+
+    let mut result = vec![];
+
+    if hundreds_digit != 0 {
+        result.push(format!("{} hundred", ones(hundreds_digit).unwrap()));
+    }
+
+    if let Some(value) = ones(n).or_else(|| teens(n)) {
+        result.push(value.to_owned());
+    } else {
+        let mut s = String::new();
+
+        if let Some(value) = teens(n - 100 * hundreds_digit) {
+            s.push_str(value);
+        } else {
+            if tens_digit != 0 {
+                s.push_str(tens(tens_digit * 10).unwrap());
+
+                if ones_digit != 0 {
+                    s.push('-');
+                }
+            }
+
+            if ones_digit != 0 {
+                s.push_str(ones(ones_digit).unwrap());
+            }
+        }
+
+        if !s.is_empty() {
+            result.push(s);
+        }
+    }
+
+    result.join(" ")
+}
+
+pub fn split_thousands(mut chars: Vec<char>) -> Vec<u64> {
+    chars.reverse();
+    chars
+        .chunks_mut(3)
+        .map(|chunk| {
+            chunk.reverse();
+            let res = chunk.iter().skip_while(|&&c| c == '0').collect::<String>();
+            if res.is_empty() {
+                0
+            } else {
+                u64::from_str(&res).unwrap()
+            }
+        })
+        .rev()
+        .collect::<Vec<_>>()
+}
+
+const SCALES: [&str; 6] = [
     "thousand",
     "million",
     "billion",
@@ -68,56 +117,56 @@ pub fn encode(n: u64) -> String {
         return "zero".to_string();
     }
 
-    let char_chunks = n.to_string().chars().rev().collect::<Vec<char>>();
-    let char_chunks = char_chunks
-        .as_slice()
-        .chunks(3)
-        .map(|chunk| chunk.iter().copied().rev().collect::<Vec<_>>());
-
-    let mut result = vec![];
-
-    for (digit_chunk, &group) in char_chunks.zip(GROUPS.iter()) {
-        let mut s = String::new();
-
-        let raw_number = u64::from_str(&digit_chunk.iter().collect::<String>()).unwrap();
-        let number = match digit_chunk.len() {
-            3 => {
-                let tens_value = raw_number - 100 * raw_number / 100;
-                format!(
-                    "{} hundred {}",
-                    ones(&raw_number / 100).unwrap(),
-                    low_numbers(tens_value).unwrap_or_else(|| {
-                        let ones_value = tens_value % 10;
-                        let tens_string = tens(10 * (tens_value / 10)).unwrap();
-                        if ones_value != 0 {
-                            format!("{}-{}", tens_string, ones(tens_value % 10).unwrap())
-                        } else {
-                            tens(10 * (tens_value / 10)).unwrap().to_owned()
-                        }
-                    })
-                )
-            }
-            2 => low_numbers(raw_number).unwrap_or_else(|| {
-                let ones_value = raw_number % 10;
-                let tens_string = tens(dbg!(10 * (raw_number / 10))).unwrap();
-                if ones_value != 0 {
-                    format!("{}-{}", tens_string, ones(raw_number % 10).unwrap())
-                } else {
-                    tens(10 * (raw_number / 10)).unwrap().to_owned()
-                }
-            }),
-            1 => {
-                format!("{}", ones(raw_number).unwrap())
-            }
-            _ => unreachable!(),
-        };
-
-        s.push_str(&number);
-
-        if !group.is_empty() {
-            s.push_str(&format!(" {}", group));
-        }
-        result.push(s);
+    if let Some(value) = ones(n).or_else(|| teens(n)) {
+        return value.to_owned();
     }
-    result.into_iter().rev().collect::<Vec<_>>().join(" ")
+
+    let s = n.to_string();
+    let splits = split_thousands(s.chars().collect::<Vec<_>>());
+    let num_groups = splits.len();
+
+    splits
+        .into_iter()
+        .map(|num| {
+            if num == 0 {
+                // use None to indicate whether we should print the suffix
+                None
+            } else {
+                Some(if let Some(value) = ones(num).or_else(|| teens(num)) {
+                    value.to_owned()
+                } else {
+                    simple(num)
+                })
+            }
+        })
+        .zip_longest(
+            SCALES[..num_groups - 1]
+                .iter()
+                .rev()
+                .map(|&s| Some(s.to_owned())),
+        )
+        .filter_map(|pair| {
+            match pair {
+                // we have a number chunk as well as a suffix that we should use
+                EitherOrBoth::Both(Some(text), Some(suffix)) => {
+                    Some(format!("{} {}", text, suffix))
+                }
+
+                // the number chunk is all zeros, we don't want to keep it in the description
+                EitherOrBoth::Both(None, Some(_)) | EitherOrBoth::Left(None) => None,
+
+                // we always have a non-None suffix if zip_longest returns a value for the right side,
+                // because there are n number chunks and n - 1 suffixes
+                EitherOrBoth::Both(_, None) => panic!("suffix should never be None"),
+
+                // No suffix happens on the final element of the iteration
+                EitherOrBoth::Left(Some(text)) => Some(text),
+
+                // The splits are guaranteed to be length 1 and at least one larger than the number of
+                // zipped suffixes
+                EitherOrBoth::Right(_) => panic!("suffix exists but number text doesn't"),
+            }
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
 }
